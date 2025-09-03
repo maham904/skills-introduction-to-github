9d890759fc9b87b652737b506100af8920ce8bcd
maham904 <mahamkhanlu@gmail.com>
2025-09-03 01:20:13 +0100

    Added cart.html and payment.html with seperate branch

--- Ecommerce/views.py
+++ Ecommerce/views.py
@@ -4,3 +4,9 @@ from django.shortcuts import render
 # Create your views here.
 def shopping_page(request):
     return render(request,'shopping.html')
+
+def cart_page(request):
+    return render(request, 'cart.html')
+
+def payment_page(request):
+    return render(request, 'payment.html')
